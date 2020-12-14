08528d4c6df79e14c70d4685bec34a61b87fc960
pond-e <56418890+pond-e@users.noreply.github.com>
2020-12-14 17:51:12 +0900

    Add files via upload

---
+++ Assets/script/CarController.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using Cinemachine;
+
+
+public class CarController : MonoBehaviour
+{
+    public Transform kartModel;
+    public Transform kartNormal;
+    public Rigidbody sphere;
+
+    public List<ParticleSystem> primaryParticles = new List<ParticleSystem>();
+    public List<ParticleSystem> secondaryParticles = new List<ParticleSystem>();
+
+    float speed, currentSpeed;
+    float rotate, currentRotate;
+    int driftDirection;
+    float driftPower;
+    int driftMode = 0;
+    bool first, second, third;
+    Color c;
+
+    [Header("Bools")]
+    public bool drifting;
+
+    [Header("Parameters")]
+
+    public float acceleration = 300f;
+    public float steering = 80f;
+    public float gravity = 10f;
+    public LayerMask layerMask;
+
+    [Header("Model Parts")]
+
+    public Transform frontWheels;
+    public Transform backWheels;
+    public Transform steeringWheel;
+
+    [Header("Particles")]
+    public Transform wheelParticles;
+    public Transform flashParticles;
+    public Color[] turboColors;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Follow Collider
+        transform.position = sphere.transform.position;
+
+        if (Input.GetButton("Fire1"))
+        {
+            speed = acceleration;
+        }
+
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            int dir = Input.GetAxis("Horizontal") > 0 ? 1 : -1;
+            float amount = Mathf.Abs((Input.GetAxis("Horizontal")));
+            Steer(dir, amount);
+        }
+
+        //Drift
+        if (Input.GetButtonDown("Jump") && !drifting && Input.GetAxis("Horizontal") != 0)
+        {
+            drifting = true;
+            driftDirection = Input.GetAxis("Horizontal") > 0 ? 1 : -1;
+        }
+
+        if (drifting)
+        {
+            float control = (driftDirection == 1) ? ExtensionMethods.Remap(Input.GetAxis("Horizontal"), -1, 1, 0, 2) : ExtensionMethods.Remap(Input.GetAxis("Horizontal"), -1, 1, 2, 0);
+            Steer(driftDirection, 1 * control);
+        }
+
+        if(Input.GetButtonUp("Jump") && drifting)
+        {
+            drifting = false;
+        }
+
+        currentSpeed = Mathf.SmoothStep(currentSpeed, speed, Time.deltaTime * 12f); speed = 0f;
+        currentRotate = Mathf.Lerp(currentRotate, rotate, Time.deltaTime * 4f); rotate = 0f;
+
+        //animations
+
+        //a) kart
+        //if (!drifting)
+        //{
+        //    kartModel.localEulerAngles = Vector3.Lerp(kartModel.localEulerAngles, new Vector3(0, 90 + (Input.GetAxis("Horizontal") * 15), kartModel.localEulerAngles.z), .2f);
+        //}
+
+
+    }
+
+
+    private void FixedUpdate()
+    {
+        //Forward Acceleration
+        if (!drifting)
+            sphere.AddForce(kartModel.transform.forward * currentSpeed, ForceMode.Acceleration);
+        else
+            sphere.AddForce(transform.forward * currentSpeed, ForceMode.Acceleration);
+
+        //Gravity
+        sphere.AddForce(Vector3.down * gravity, ForceMode.Acceleration);
+
+        //Steering
+        transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, new Vector3(0, transform.eulerAngles.y + currentRotate, 0), Time.deltaTime * 5f);
+
+        RaycastHit hitOn;
+        RaycastHit hitNear;
+
+        Physics.Raycast(transform.position + (transform.up * .1f), Vector3.down, out hitOn, 1.1f, layerMask);
+        Physics.Raycast(transform.position + (transform.up * .1f), Vector3.down, out hitNear, 2.0f, layerMask);
+
+        //Normal Rotation
+
+    }
+
+    public void Steer(int direction, float amount)
+    {
+        rotate = (steering * direction) * amount;
+    }
+}
