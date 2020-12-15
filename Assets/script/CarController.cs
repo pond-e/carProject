4aff13b2be837553842a5a151b0e524bad7fa4cb
pond-e <haruto2266@icloud.com>
2020-12-15 21:58:08 +0900

    first commit

--- Assets/script/CarController.cs
+++ Assets/script/CarController.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
@@ -11,9 +11,6 @@ public class CarController : MonoBehaviour
     public Transform kartNormal;
     public Rigidbody sphere;
 
-    public List<ParticleSystem> primaryParticles = new List<ParticleSystem>();
-    public List<ParticleSystem> secondaryParticles = new List<ParticleSystem>();
-
     float speed, currentSpeed;
     float rotate, currentRotate;
     int driftDirection;
@@ -22,26 +19,16 @@ public class CarController : MonoBehaviour
     bool first, second, third;
     Color c;
 
-    [Header("Bools")]
-    public bool drifting;
-
     [Header("Parameters")]
 
-    public float acceleration = 300f;
+    public float acceleration = 30f;
     public float steering = 80f;
     public float gravity = 10f;
     public LayerMask layerMask;
+    
+    Vector3 _moveVector = new Vector3(0f, 0f, 0f);                  // 移動速度の入力
+    public float moveForceMultiplier = 1f;     // 移動速度の入力に対する追従度
 
-    [Header("Model Parts")]
-
-    public Transform frontWheels;
-    public Transform backWheels;
-    public Transform steeringWheel;
-
-    [Header("Particles")]
-    public Transform wheelParticles;
-    public Transform flashParticles;
-    public Color[] turboColors;
     // Start is called before the first frame update
     void Start()
     {
@@ -53,10 +40,15 @@ public class CarController : MonoBehaviour
     {
         //Follow Collider
         transform.position = sphere.transform.position;
+        
 
         if (Input.GetButton("Fire1"))
         {
             speed = acceleration;
+        }
+        if (Input.GetButton("Fire2"))
+        {
+            
         }
 
         if (Input.GetAxis("Horizontal") != 0)
@@ -66,35 +58,8 @@ public class CarController : MonoBehaviour
             Steer(dir, amount);
         }
 
-        //Drift
-        if (Input.GetButtonDown("Jump") && !drifting && Input.GetAxis("Horizontal") != 0)
-        {
-            drifting = true;
-            driftDirection = Input.GetAxis("Horizontal") > 0 ? 1 : -1;
-        }
-
-        if (drifting)
-        {
-            float control = (driftDirection == 1) ? ExtensionMethods.Remap(Input.GetAxis("Horizontal"), -1, 1, 0, 2) : ExtensionMethods.Remap(Input.GetAxis("Horizontal"), -1, 1, 2, 0);
-            Steer(driftDirection, 1 * control);
-        }
-
-        if(Input.GetButtonUp("Jump") && drifting)
-        {
-            drifting = false;
-        }
-
         currentSpeed = Mathf.SmoothStep(currentSpeed, speed, Time.deltaTime * 12f); speed = 0f;
         currentRotate = Mathf.Lerp(currentRotate, rotate, Time.deltaTime * 4f); rotate = 0f;
-
-        //animations
-
-        //a) kart
-        //if (!drifting)
-        //{
-        //    kartModel.localEulerAngles = Vector3.Lerp(kartModel.localEulerAngles, new Vector3(0, 90 + (Input.GetAxis("Horizontal") * 15), kartModel.localEulerAngles.z), .2f);
-        //}
-
 
     }
 
@@ -102,10 +67,7 @@ public class CarController : MonoBehaviour
     private void FixedUpdate()
     {
         //Forward Acceleration
-        if (!drifting)
-            sphere.AddForce(kartModel.transform.forward * currentSpeed, ForceMode.Acceleration);
-        else
-            sphere.AddForce(transform.forward * currentSpeed, ForceMode.Acceleration);
+        sphere.AddForce(kartModel.transform.forward * currentSpeed, ForceMode.Force);
 
         //Gravity
         sphere.AddForce(Vector3.down * gravity, ForceMode.Acceleration);
@@ -119,7 +81,6 @@ public class CarController : MonoBehaviour
         Physics.Raycast(transform.position + (transform.up * .1f), Vector3.down, out hitOn, 1.1f, layerMask);
         Physics.Raycast(transform.position + (transform.up * .1f), Vector3.down, out hitNear, 2.0f, layerMask);
 
-        //Normal Rotation
 
     }
 
@@ -128,3 +89,4 @@ public class CarController : MonoBehaviour
         rotate = (steering * direction) * amount;
     }
 }
+
